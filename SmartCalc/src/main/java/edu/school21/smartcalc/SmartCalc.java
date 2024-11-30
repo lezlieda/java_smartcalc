f241e6e8d31da9aab62279bc43c0d3e9a37662d8
lezlieda <lezlieda@student.21-school.ru>
2024-12-01 00:28:28 +0300

    отрисовал морду основного калькулятора

---
+++ SmartCalc/src/main/java/edu/school21/smartcalc/SmartCalc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.school21.smartcalc;
+
+/**
+ *
+ * @author user
+ */
+public class SmartCalc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
